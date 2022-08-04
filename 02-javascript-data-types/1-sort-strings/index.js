8e0e0fdd9a2cdc3350e40a098325e3ab4d773707
Gerandum2008 <gerandum2008@ro.ru>
2022-08-04 20:30:21 +0300

    исправление sortStrings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,12 +4,10 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = "asc") {
-  const arrClone = [...arr].sort((a, b) => {
-    return a.localeCompare(b, ["ru-RU", "en-US"], { caseFirst: "upper" });
-  });
-  if (param === "asc") {
-    return arrClone;
-  }
-  return arrClone.reverse();
-}
+export  function sortStrings(arr, param = "asc") {
+  return [...arr].sort((a, b) => {
+     if (param === "asc") {
+     return a.localeCompare(b, ["ru-RU", "en-US"], { caseFirst: "upper" })}
+    return b.localeCompare(a, ["ru-RU", "en-US"], { caseFirst: "upper" })
+   });
+ }
