736ea3c96ebdd26167562fdca441e9307b6aa4f9
Gerandum2008 <gerandum2008@ro.ru>
2022-08-07 17:50:38 +0300

    3 модуль

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,14 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
-
+export function createGetter(field) {
+  let arr = field.split(".");
+  return (evt) => {
+    let res = evt;
+    for (const key of arr) {
+      if (res === undefined) break;
+      res = res[key];
+    }
+    return res;
+  };
 }
--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,12 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
-
+  if (arr === undefined) return [];
+  if (!arr.length) return [];
+  return arr.reduce((val, key) => {
+    if (!val.includes(key)) {
+      val.push(key);
+    }
+    return val;
+  }, []);
 }
