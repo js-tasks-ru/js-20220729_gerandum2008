2c6c5d30aa51cb6dd170929c24749d837e79d294
Gerandum2008 <gerandum2008@ro.ru>
2022-08-11 21:54:22 +0300

    модуль 4

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,78 @@
 export default class ColumnChart {
+  element;
+  chartHeight = 50;
+  constructor({
+    data = [],
+    label = "",
+    value = 0,
+    link = "",
+    formatHeading = (data) => data,
+  } = {}) {
+    this.data = data;
+    this.label = label;
+    this.value = formatHeading(value);
+    this.link = link;
+    this.render();
+  }
+  get blockColum() {
+    return `<div class="column-chart column-chart_loading" style="--chart-height: ${
+      this.chartHeight
+    }">
+      <div class="column-chart__title">
+        Total ${this.label}
+        ${this.getLink()}
+      </div>
+      ${this.getColumChartsCont()}
+    </div>`;
+  }
+  getLink() {
+    return this.link
+      ? `<a class="column-chart__link" href="${this.link}">View all</a>`
+      : "";
+  }
+  getHeader() {
+    return `<div data-element="header" class="column-chart__header">${this.value}</div>`;
+  }
+  getBody() {
+    return `<div data-element="body" class="column-chart__chart">${this.getCharts(
+      this.data
+    )}</div>`;
+  }
+  getColumChartsCont() {
+    return `<div class="column-chart__container">
+        ${this.getHeader()}
+        ${this.getBody()}
+      </div>`;
+  }
+  getCharts(data) {
+    const maxData = Math.max(...data);
+    const scale = this.chartHeight / maxData;
+    return data
+      .map((key) => {
+        return `<div style="--value: ${Math.floor(
+          key * scale
+        )}" data-tooltip="${((key / maxData) * 100).toFixed(0)}%"></div>`;
+      })
+      .join("");
+  }
+  render() {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = this.blockColum;
+    this.element = wrapper.firstElementChild;
 
+    if (this.data.length) {
+      this.element.classList.remove("column-chart_loading");
+    }
+  }
+  update(data) {
+    this.getCharts(data);
+  }
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+  destroy() {
+    this.remove();
+  }
 }
